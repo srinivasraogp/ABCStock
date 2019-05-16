1e4cac52af1e4c503b259efc7ea2c592b97b5a8b
nithin2889 <nithin2889@gmail.com>
2019-05-16 18:47:22 +0530

    user service

--- src/main/java/com/hcl/abcstock/services/UserService.java
+++ src/main/java/com/hcl/abcstock/services/UserService.java
@@ -2,6 +2,7 @@ package com.hcl.abcstock.services;
 
 import java.util.List;
 
+import com.hcl.abcstock.models.Transaction;
 import com.hcl.abcstock.models.User;
 import com.hcl.abcstock.beans.*;
 import com.hcl.abcstock.errors.StockNotAvailableException;
@@ -11,4 +12,5 @@ public interface UserService {
 	List<User> findAllUsers();
 	public String processPurchase(StockPurchaseRequestDto stockPurchaseRequest) throws StockNotAvailableException;
 	public double percentageOf(double stockPrice,double stockPercentage);
+	List<Transaction> getAllTransationForUser(int userId);
 }
