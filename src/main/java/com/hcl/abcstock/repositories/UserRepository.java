23a9b62cdd2baf269736978b6d0ed3903b52dfde
bandisravan <sravankumar0000@gmail.com>
2019-05-16 14:32:23 +0530

    repository added

--- src/main/java/com/hcl/abcstock/repositories/UserRepository.java
+++ src/main/java/com/hcl/abcstock/repositories/UserRepository.java
@@ -1,5 +1,11 @@
 package com.hcl.abcstock.repositories;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hcl.abcstock.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
