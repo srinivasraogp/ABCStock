d30312751eef9cac8b9039aa7e39ad214449b2bd
nithin2889 <nithin2889@gmail.com>
2019-05-16 15:01:00 +0530

    user entity

--- src/main/java/com/hcl/abcstock/models/User.java
+++ src/main/java/com/hcl/abcstock/models/User.java
@@ -1,5 +1,7 @@
 package com.hcl.abcstock.models;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,7 +11,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table
-public class User {
+public class User implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int userId;
